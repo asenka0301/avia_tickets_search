1456bcb05f09c01716d30b0a216fdd1bacecf29c
Anastasia Simonova <asenka0301@gmail.com>
2023-12-16 11:31:32 +0300

    feat: add data inputs

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './Form.module.css';
 
 function Form() {
+  const refTo = useRef();
+  const refFrom = useRef();
   return (
     <form className={styles.form}>
       <input className={styles.input} type="text" placeholder="Откуда" />
       <input className={styles.input} type="text" placeholder="Куда" />
+      <input
+        ref={refTo}
+        className={styles.input}
+        placeholder="Когда"
+        onFocus={() => refTo.current.type = 'date'}
+        onBlur={() => refTo.current.type = 'text'}
+      />
+      <input
+        ref={refFrom}
+        className={styles.input}
+        placeholder="Обратно"
+        onFocus={() => refFrom.current.type = 'date'}
+        onBlur={() => refFrom.current.type = 'text'}
+      />
       <button type="submit">Найти билеты</button>
     </form>
   );
